021521307f9399f378a08dee4e1927f56766bfd1
nirpechuk <npechuk@gmail.com>
2023-12-29 21:15:03 -0800

    added features: - checking for validity that breaks the system and throwing errors if it does. next: - checking for validity that doesn't break the system (eg no rubric or assignment due before feedback due)

--- teams_messages.py
+++ teams_messages.py
@@ -1,10 +1,60 @@
 import pymsteams
 
+WEBHOOK = "https://davidsononline.webhook.office.com/webhookb2/53ebf1e7-4890-4eaf-a5e5-1d41cac760e1@0ec5874b-c352" \
+          "-49ff-ba00-40ef1998e526/IncomingWebhook/0b3f190df98149c48714885142e1bb23/2b91f670-f44c-4d36-925a" \
+          "-6b2696eb7d4c "
 
-def send_message(name, email, assignment, week, due):
-    myTeamsMessage = pymsteams.connectorcard(
-        "https://davidsononline.webhook.office.com/webhookb2/53ebf1e7-4890-4eaf-a5e5-1d41cac760e1@0ec5874b-c352-49ff-ba00"
-        "-40ef1998e526/IncomingWebhook/0b3f190df98149c48714885142e1bb23/2b91f670-f44c-4d36-925a-6b2696eb7d4c")
+
+def invalid_message(error, assignment, week, debug=False):
+    name = "Nir Pechuk"
+    email = "npechuk@davidsononline.org"
+    myTeamsMessage = pymsteams.connectorcard(WEBHOOK)
+    myTeamsMessage.payload = {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": {
+                    "type": "AdaptiveCard",
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "size": "Large",
+                            "weight": "Bolder",
+                            "text": "Invalid Submission"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "wrap": "true",
+                            "text": f"<at>{name}</at>, {assignment}, Week {week}, Error: {error}"
+                        }
+                    ],
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "version": "1.0",
+                    "msteams": {
+                        "entities": [
+                            {
+                                "type": "mention",
+                                "text": f"<at>{name}</at>",
+                                "mentioned": {
+                                    "id": f"{email}",
+                                    "name": f"{name}"
+                                }
+                            },
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+    if debug:
+        myTeamsMessage.printme()
+    else:
+        myTeamsMessage.send()
+
+
+def assignment_message(name, email, assignment, week, due, debug=False):
+    myTeamsMessage = pymsteams.connectorcard(WEBHOOK)
 
     myTeamsMessage.payload = {
         "type": "message",
@@ -16,13 +66,14 @@ def send_message(name, email, assignment, week, due):
                     "body": [
                         {
                             "type": "TextBlock",
-                            "size": "Medium",
+                            "size": "Large",
                             "weight": "Bolder",
                             "text": "New Assignment"
                         },
                         {
                             "type": "TextBlock",
-                            "text": f"<at>{name}</at>, {assignment}, Week {week}, Feedback Due {due}"
+                            "wrap": "true",
+                            "text": f"<at>{name}</at>, {assignment}, Week {week}, Feedback Due {due} @ 4:00 pm"
                         }
                     ],
                     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
@@ -44,6 +95,7 @@ def send_message(name, email, assignment, week, due):
         ]
     }
 
-    myTeamsMessage.printme()
-
-    # myTeamsMessage.send()
+    if debug:
+        myTeamsMessage.printme()
+    else:
+        myTeamsMessage.send()
