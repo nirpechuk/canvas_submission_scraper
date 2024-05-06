ff9cdbf93bfa79e7b523ee2064b64d7271172128
nirpechuk <33583056+nirpechuk@users.noreply.github.com>
2024-05-06 08:03:12 -0700

    Update teams_messages.py

--- teams_messages.py
+++ teams_messages.py
@@ -1,13 +1,11 @@
 import pymsteams
 
-WEBHOOK = "https://davidsononline.webhook.office.com/webhookb2/53ebf1e7-4890-4eaf-a5e5-1d41cac760e1@0ec5874b-c352" \
-          "-49ff-ba00-40ef1998e526/IncomingWebhook/0b3f190df98149c48714885142e1bb23/2b91f670-f44c-4d36-925a" \
-          "-6b2696eb7d4c "
+WEBHOOK = "" #insert webhook here
 
 
 def invalid_message(error, assignment, week, debug=False):
-    name = "Nir Pechuk"
-    email = "npechuk@davidsononline.org"
+    name = "" # [your name]
+    email = "" # [your email]
     myTeamsMessage = pymsteams.connectorcard(WEBHOOK)
     myTeamsMessage.payload = {
         "type": "message",
